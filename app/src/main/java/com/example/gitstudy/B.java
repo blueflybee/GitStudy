48fc1e010979c088f76096cf3fa16d259b495bbb
shaojun <wusj2017@gmail.com>
2017-07-19 15:47:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gitstudy/B.java
@@ -0,0 +1,15 @@
+package com.example.gitstudy;
+
+/**
+ * <pre>
+ *     author : shaojun
+ *     e-mail : wusj@qtec.cn
+ *     time   : 2017/07/14
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class B {
+
+
+}
---
+++ app/src/main/java/com/example/gitstudy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.gitstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  private String mString = "";
+  private int mInt = 0;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    //
+
+//asdfasdf
+
+  }
+}
